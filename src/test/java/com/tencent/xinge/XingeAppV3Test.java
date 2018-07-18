4bb4572537cda113cceb7d5857aa28b8a910430f
ablecao <ablecao@tencent.com>
2018-07-18 13:55:54 +0800

    修改UT

--- src/test/java/com/tencent/xinge/XingeAppV3Test.java
+++ src/test/java/com/tencent/xinge/XingeAppV3Test.java
@@ -1,5 +1,6 @@
 package com.tencent.xinge;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,49 +12,52 @@ import com.tencent.xinge.request.PushAppRequest;
 
 public class XingeAppV3Test {
 
-	private XingeAppV3 xinge = null;
-	private String appId = "a5b5f071ff77a";
-	private String secretKey = "ea512eb7704d5fb5a6a937cafa70e771";
+    private XingeAppV3 xinge = null;
+    private String appId = "a5b5f071ff77a";
+    private String secretKey = "ea512eb7704d5fb5a6a937cafa70e771";
 
-	@Before
-	public void init() {
-		xinge = new XingeAppV3(appId, secretKey);
-	}
+    @Before
+    public void init() {
+        xinge = new XingeAppV3(appId, secretKey);
+    }
 
-	@Test
-	public void testPushAppAndroid() {
+    @Test
+    public void testPushAppAndroid() {
 
-		MessageAndroid androidMessage = new MessageAndroid();
-		androidMessage.setType(1);
-		androidMessage.setTitle("xxx");
-		androidMessage.setContent("xxxxxxxxx");
-		TimeInterval acceptTime1 = new TimeInterval(13, 00, 14, 00);
-		TimeInterval acceptTime2 = new TimeInterval(0, 00, 9, 00);
-		androidMessage.addAcceptTime(acceptTime1);
-		androidMessage.addAcceptTime(acceptTime2);
+        MessageAndroid androidMessage = new MessageAndroid();
+        androidMessage.setType(1);
+        androidMessage.setTitle("xxx");
+        androidMessage.setContent("xxxxxxxxx");
+        TimeInterval acceptTime1 = new TimeInterval(13, 00, 14, 00);
+        TimeInterval acceptTime2 = new TimeInterval(0, 00, 9, 00);
+        androidMessage.addAcceptTime(acceptTime1);
+        androidMessage.addAcceptTime(acceptTime2);
 
-		ClickAction action = new ClickAction();
-		action.setActionType(0);
-		action.setActivity("xxx");
-		action.setIntent("xxx");
-		action.setActionType(0);
-		androidMessage.setAction(action);
+        ClickAction action = new ClickAction();
+        action.setActionType(0);
+        action.setActivity("xxx");
+        action.setIntent("xxx");
+        action.setActionType(0);
+        androidMessage.setAction(action);
 
-		Style style = new Style(0, 1, 1, 1, 0, 1, 0, 1);
-		androidMessage.setStyle(style);
+        Style style = new Style(0, 1, 1, 1, 0, 1, 0, 1);
+        androidMessage.setStyle(style);
 
-		PushAppRequest pushAppRequest = new PushAppRequest("all", "android", "notify", androidMessage);
-		pushAppRequest.setSeq(123);
-		pushAppRequest.setEnvironment("product");
+        PushAppRequest pushAppRequest = new PushAppRequest("all", "android", "notify", androidMessage);
+        pushAppRequest.setSeq(123);
+        pushAppRequest.setEnvironment("product");
 
-		System.out.println(pushAppRequest.toString());
+        String jsonStr = "{\"audience_type\":\"all\",\"message_type\":\"notify\"," +
+                "\"message\":{\"clearable\":1,\"ring\":1,\"vibrate\":1,\"title\":\"xxx\",\"content\":\"xxxxxxxxx\"," +
+                "\"builder_id\":0,\"action\":{\"activity\":\"xxx\",\"action_type\":0," +
+                "\"browser\":{\"confirm\":0,\"url\":\"\"}," +
+                "\"intent\":\"xxx\",\"aty_attr\":{\"pf\":0,\"if\":0}},\"style_id\":1,\"custom_content\":{}," +
+                "\"accept_time\":[{\"start\":{\"min\":\"0\",\"hour\":\"13\"},\"end\":{\"min\":\"0\",\"hour\":\"14\"}}," +
+                "{\"start\":{\"min\":\"0\",\"hour\":\"0\"},\"end\":{\"min\":\"0\",\"hour\":\"9\"}}]," +
+                "\"lights\":1,\"n_id\":0,\"icon_type\":0},\"platform\":\"android\"}";
+
+        Assert.assertEquals(jsonStr, pushAppRequest.toString());
 //		System.out.println(xinge.pushApp(pushAppRequest.toString()));
-	}
+    }
 
-	@Test
-	public void testTagAndroid() {
-		// '{"operator_type":1,"op_type":"qq","op_id":"2948181869","seq":1526903924,"platform":"android","token_list":["17f3edd107fd85bf8037a6c67437ba7838b1e5aa","9c82f0ac382e2a24671a3f8c3079bab5c726ad37"]}'
-		// {"seq":1526903924,","ret_code":10102,"err_msg":"缺少参数tag_list，请检查后重试!"}
-
-	}
 }
