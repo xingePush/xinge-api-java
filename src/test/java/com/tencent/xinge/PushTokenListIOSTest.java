edd230079ab3010f7aa826b9eda2eb0ef2c0d260
jakkzhang <jakkzhang>
2018-07-19 11:23:41 +0800

    add token list push ut

--- src/test/java/com/tencent/xinge/PushTokenListIOSTest.java
+++ src/test/java/com/tencent/xinge/PushTokenListIOSTest.java
@@ -1,12 +1,49 @@
 package com.tencent.xinge;
 
+import com.tencent.xinge.bean.*;
 import org.junit.Test;
 
-public class PushTokenListIOSTest extends BaseXgTest {
+import java.util.ArrayList;
+import com.tencent.xinge.push.app.PushAppRequest;
 
+//token list推送
+public class PushTokenListIOSTest extends BaseXgTest {
 
     @Test
     public void testPushTokenListIOS() {
-        //TODO
+        Message message = new Message();
+        message.setContent("testPushTokenListIOS from Java API");
+        message.setTitle("testPushTokenListIOS");
+
+
+        MessageIOS msgIOS = new MessageIOS();
+        Alert alert = new Alert();
+        alert.setTitle("alert from Java API");
+        alert.setBody(" body alert from Java API");
+
+        Aps aps = new Aps();
+        aps.setBadge(8);
+        aps.setAlert(alert);
+        msgIOS.setAps(aps);
+
+        message.setIos(msgIOS);
+
+        ArrayList<String> tokenList = new ArrayList<>();
+        tokenList.add("0d7b5ab1afe4b58973b02a8c80caea51ad6e51f517de6ead3b42576653a69457");
+        tokenList.add("22a3bad291f1a8028d0468ecc00eb7f3818b859b7ec3a0f8b8f584a22d938cf2");
+
+        PushAppRequest pushAppRequest = new PushAppRequest();
+        pushAppRequest.setPlatform(Platform.ios);
+        pushAppRequest.setEnvironment(Environment.dev);
+        pushAppRequest.setMessage_type(MessageType.notify);
+        pushAppRequest.setMessage(message);
+        pushAppRequest.setSend_time("1111111");
+        pushAppRequest.setStat_tag("test");
+        pushAppRequest.setPush_id("0");
+        pushAppRequest.setToken_list(tokenList);
+        pushAppRequest.setAudience_type(AudienceType.token_list);
+
+        System.out.println(pushAppRequest.toString());
+        System.out.println(xingeIOS.pushApp(pushAppRequest.toString()));
     }
 }
