bfcab159008ae3a0a28b67bbeb5413f6a40a122a
ablecao <ablecao@tencent.com>
2018-07-19 10:07:22 +0800

    add UT PushTagIOSTest

---
+++ src/test/java/com/tencent/xinge/PushTagIOSTest.java
@@ -0,0 +1,12 @@
+package com.tencent.xinge;
+
+import org.junit.Test;
+
+public class PushTagIOSTest extends BaseXgTest {
+
+    @Test
+    public void testPushTagIOS() {
+        //TODO
+
+    }
+}
