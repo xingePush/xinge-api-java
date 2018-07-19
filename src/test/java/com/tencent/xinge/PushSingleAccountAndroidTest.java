aea7483a2edc30c9f99dda18f7bd63885baca8cd
ablecao <ablecao@tencent.com>
2018-07-19 12:09:06 +0800

    fix UT PushSingleAccountAndroidTest

--- src/test/java/com/tencent/xinge/PushSingleAccountAndroidTest.java
+++ src/test/java/com/tencent/xinge/PushSingleAccountAndroidTest.java
@@ -1,12 +1,60 @@
 package com.tencent.xinge;
 
+import com.tencent.xinge.bean.*;
+import com.tencent.xinge.push.app.PushAppRequest;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class PushSingleAccountAndroidTest extends BaseXgTest {
 
 
     @Test
     public void testPushSingleAccountAndroid() {
-        //TOOD
+
+
+        MessageAndroid messageAndroid = new MessageAndroid();
+
+        ClickAction action = new ClickAction();
+        action.setActionType(0);
+        action.setActivity("xxx");
+        action.setIntent("xxx");
+        action.setActionType(0);
+
+        messageAndroid.setAction(action);
+        Style style = new Style(0, 1, 1, 1, 0, 1, 0, 1);
+
+        messageAndroid.setBuilder_id(0);
+        messageAndroid.setRing(1);
+        messageAndroid.setVibrate(1);
+        messageAndroid.setClearable(1);
+        messageAndroid.setN_id(0);
+        messageAndroid.setLights(1);
+        messageAndroid.setIcon_type(0);
+        messageAndroid.setStyle_id(1);
+
+        Message message = new Message();
+        message.setAndroid(messageAndroid);
+        message.setTitle("testPushSingleAccountAndroid");
+        message.setContent("from Java SDK");
+
+
+        PushAppRequest pushAppRequest = new PushAppRequest();
+        pushAppRequest.setEnvironment(Environment.product);
+        pushAppRequest.setMessage_type(MessageType.notify);
+        pushAppRequest.setPlatform(Platform.android);
+        pushAppRequest.setAudience_type(AudienceType.account);
+        pushAppRequest.setSeq(123);
+        pushAppRequest.setPush_id("0");
+        pushAppRequest.setSend_time("1111111");
+        pushAppRequest.setStat_tag("test");
+
+        ArrayList<String> accountList = new ArrayList<>();
+        accountList.add("agtestAccount");
+        pushAppRequest.setAccount_list(accountList);
+
+        System.out.println(pushAppRequest.toString());
+        System.out.println(xingeAndroid.pushApp(pushAppRequest.toString()));
+
     }
 }
